aa6ffc41955ec320289b4a997082b60855bc6a1a
cp8520 <cp85208520@gmail.com>
2024-03-09 16:47:20 -0700

    fix copy/paste logic. It was iterating through all the fields before and settling on the last field. Now, it will choose the currently selected tab and copy from that field. It will do the same in order to paste.

--- eBim.Scribe.py
+++ eBim.Scribe.py
@@ -110,23 +110,17 @@ def main(page: ft.Page):
     tabs_index = []
     input_fields = []
 
-    def manipulate_input_field_copy(event,index):
-        selected_index = index
-        if 0 <= selected_index < len(input_fields):
-            current_input_field = tabs_control.selected_index
-            for current_input_field in input_fields:
-                pyperclip.copy(current_input_field.value)
-                create_input_field.value = "Copied"
-                page.update()
+    def manipulate_input_field_copy(event, index):
+        if 0 <= index < len(input_fields):
+            pyperclip.copy(input_fields[index].value)
+            create_input_field.value = "Copied"
+            page.update()
 
-    def manipulate_input_field_paste(event,index):
-        selected_index = index
-        if 0 <= selected_index < len(input_fields):
-            tabs_control.selected_index = tabs_control.selected_index
-            for current_input_field in input_fields:
-                input_fields[tabs_control.selected_index].value = None
-                input_fields[tabs_control.selected_index].value = pyperclip.paste()
-                page.update()
+    def manipulate_input_field_paste(event, index):
+        if 0 <= index < len(input_fields):
+            input_fields[index].value = pyperclip.paste()
+            page.update()
+
 
             
 
