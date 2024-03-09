26f6c4e07a4fc2b4a74c293d79570984105a8483
cp8520 <cp85208520@gmail.com>
2024-03-09 02:17:07 -0700

    Feature added: Snack tray with copy and paste feature from legacy version

--- eBim.Scribe.py
+++ eBim.Scribe.py
@@ -31,10 +31,10 @@ def main(page: ft.Page):
         page.snack_bar = ft.SnackBar(ft.Row([
             # ft.IconButton(ft.icons.ADD,on_click=count_up),
             # ft.IconButton(ft.icons.REMOVE,on_click=count_down),
-            ft.IconButton(ft.icons.SELECT_ALL),
-            ft.IconButton(ft.icons.COPY),
-            ft.IconButton(ft.icons.CUT),
-            ft.IconButton(ft.icons.PASTE),
+            # ft.IconButton(ft.icons.SELECT_ALL,on_click=lambda e: manipulate_input_field_copy(e,tabs_control.selected_index)),
+            ft.IconButton(ft.icons.COPY,on_click=lambda e: manipulate_input_field_copy(e,tabs_control.selected_index)),
+            # ft.IconButton(ft.icons.CUT),
+            ft.IconButton(ft.icons.PASTE,on_click=lambda e: manipulate_input_field_paste(e,tabs_control.selected_index)),
             
         ]))
         page.snack_bar.open = True
@@ -61,13 +61,34 @@ def main(page: ft.Page):
             min_lines=20,max_lines=20, autocorrect=True, hint_text="Start scribing!",
             enable_suggestions=True, multiline=True,shift_enter=True, on_submit=insert_timestamp,show_cursor=True,on_focus=insert_timestamp
             )
+        input_fields.append(input_field)
         return input_field
     def check_tab_index(event,index):
         print(f"CLICKED & Index={index}")
 
     tabs_list = []
     tabs_index = []
-    
+    input_fields = []
+
+    def manipulate_input_field_copy(event,index):
+        selected_index = index
+        if 0 <= selected_index < len(input_fields):
+            current_input_field = input_fields[selected_index]
+            for current_input_field in input_fields:
+                pyperclip.copy(current_input_field.value)
+                create_input_field.value = "Copied"
+                page.update()
+
+    def manipulate_input_field_paste(event,index):
+        selected_index = index
+        if 0 <= selected_index < len(input_fields):
+            current_input_field = input_fields[selected_index]
+            for current_input_field in input_fields:
+                current_input_field.value = None
+                current_input_field.value = pyperclip.paste()
+                page.update()
+
+            
 
     def add_tab(event):
         t = build_tab()
@@ -80,6 +101,7 @@ def main(page: ft.Page):
         tabs_control.tabs = tabs_list  
         try:
             tabs_list.pop(index)
+            tabs_control.selected_index = len(tabs_list)-1
         except:
             tabs_list.pop()
         page.update()
@@ -108,10 +130,10 @@ def main(page: ft.Page):
     def build_tab():
         new_tab = ft.Tab(text=f"eBim {len(tabs_list)+1}",  content=ft.Column([            
             create_input_field(),
-            # ft.Row([            
-            #     # ft.Text(f"Count: {d.counter}"),
-            #     ft.Text(f" "),
-            #     ft.IconButton(ft.icons.ABC,on_click=lambda e: open_snackbar(e))]),
+            ft.Row([            
+                # ft.Text(f"Count: {d.counter}"),
+                ft.Text(f"Tools: "),
+                ft.IconButton(ft.icons.ABC,on_click=lambda e: open_snackbar(e))]),
 
             ]))
         return new_tab
